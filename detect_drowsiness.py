2a66a6e5838ca97775f5318c318e4bc4c6586abe
Ashish <ashishpandey2062@gmail.com>
2024-02-08 23:25:33 +0545

    update

--- detect_drowsiness.py
+++ detect_drowsiness.py
@@ -29,15 +29,18 @@ while True:
     _, frame = cap.read()
     height = frame.shape[0]
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 2)
+    # print(faces)
     for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 1)
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (12,243, 8), 2)
         roi_gray = gray[y:y+h, x:x+w]
+        # print(roi_gray)
         roi_color = frame[y:y+h, x:x+w]
+        # print(roi_color)
         left_eye = left_eye_cascade.detectMultiScale(roi_gray)
         right_eye = right_eye_cascade.detectMultiScale(roi_gray)
         for (x1, y1, w1, h1) in left_eye:
-            cv2.rectangle(roi_color, (x1, y1), (x1 + w1, y1 + h1), (0, 255, 0), 1)
+            cv2.rectangle(roi_color, (x1, y1), (x1 + w1, y1 + h1), (8, 118, 243), 2)
             eye1 = roi_color[y1:y1+h1, x1:x1+w1]
             eye1 = cv2.resize(eye1, (145, 145))
             eye1 = eye1.astype('float') / 255.0
@@ -50,7 +53,7 @@ while True:
             break
 
         for (x2, y2, w2, h2) in right_eye:
-            cv2.rectangle(roi_color, (x2, y2), (x2 + w2, y2 + h2), (0, 255, 0), 1)
+            cv2.rectangle(roi_color, (x2, y2), (x2 + w2, y2 + h2), (43, 255, 23), 1)
             eye2 = roi_color[y2:y2 + h2, x2:x2 + w2]
             eye2 = cv2.resize(eye2, (145, 145))
             eye2 = eye2.astype('float') / 255.0
@@ -64,7 +67,7 @@ while True:
 
         # If the eyes are closed, start counting
         if status1 == 2 and status2 == 2:
-        #if pred1 == 2 and pred2 == 2:
+        
             count += 1
             cv2.putText(frame, "Eyes Closed, Frame count: " + str(count), (10, 30), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 1)
             # if eyes are closed for 10 consecutive frames, start the alarm
